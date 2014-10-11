bf8e25a5711d99532a171a24313f4ec01b2f7fb8
Harry <hjwp2@cantab.net>
2014-10-11 12:34:48 +0100

    simplify some stuff in ft

--- functional_tests/test_site.py
+++ functional_tests/test_site.py
@@ -1,15 +1,8 @@
-import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from reproduction.models import TestModel
 
 
-class FunctionalTest(StaticLiveServerTestCase):
-    pass
-
-
-class TestModelTest(FunctionalTest):
+class TestModelTest(StaticLiveServerTestCase):
     def test_models_are_there(self):
         self.assertEqual(TestModel.objects.count(), 4)
 
