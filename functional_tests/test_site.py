c6852a8e96734e788ab3aa0d04a53424c6fac0fa
Ruben Nielsen <ruben.nielsen@mosek.com>
2014-10-16 12:26:12 +0200

    Adds the lines that fix the issue

--- functional_tests/test_site.py
+++ functional_tests/test_site.py
@@ -3,6 +3,9 @@ from reproduction.models import TestModel
 
 
 class TestModelTest(StaticLiveServerTestCase):
+    serialized_rollback = True
+    available_apps = ['reproduction']
+    
     def test_models_are_there(self):
         self.assertEqual(TestModel.objects.count(), 4)
 
